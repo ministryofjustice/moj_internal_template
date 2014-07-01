c3962a67e0705436b8e71af60c2014f9dbe3fa3b
Paul Battley <pbattley@gmail.com>
2014-07-01 17:32:55 +0100

    Make fonts work on Heroku

--- lib/moj_internal_template/engine.rb
+++ lib/moj_internal_template/engine.rb
@@ -2,13 +2,11 @@ module MojInternalTemplate
   class Engine < ::Rails::Engine
     initializer "moj_internal_template.assets.precompile" do |app|
       app.config.assets.paths << Engine.root.join("app", "assets", "fonts")
-      app.config.assets.precompile += %w(
-        moj-internal*.css
-        moj.js
-        *.woff
-        *.ttf
-        *.svg
-      )
+      app.config.assets.precompile += [
+        /moj-internal.*\.css$/,
+        /moj\.js$/,
+        /\.(?:svg|eot|woff|ttf)$/
+      ]
     end
   end
 end
