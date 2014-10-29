bf8b13cd6f29aea4b47679538de13d4d2c7a7578
Toby Privett <toby@snaplab.co.uk>
2014-10-29 10:27:16 +0000

    Enable the client app to disable link from logo
    - Accessibility report for the people finder 
    suggested that it is better not to have a link to 
    the root page when the user is on that page
    
    - This can be achieved by setting: 
    @disable_logo_link  = true

--- app/helpers/moj_helper.rb
+++ app/helpers/moj_helper.rb
@@ -2,4 +2,10 @@ module MojHelper
   def config_item(key)
     Rails.configuration.send(key)
   end
+
+  def logo_image
+    image_tag('moj_logo_horizontal_36x246.png',
+      class: 'content',
+      width: 36, height: 246, alt: 'Ministry of Justice')
+  end
 end
